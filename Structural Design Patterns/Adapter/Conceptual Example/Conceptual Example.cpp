ca8909b6a65c287fafa29f38855c7923f5659b44
Biencredible <fabian.bien@gmx.de>
2023-02-24 08:01:51 +0100

    Added Conceptual Example from refactoring.guru

---
+++ Structural Design Patterns/Adapter/Conceptual Example/Conceptual Example.cpp
@@ -0,0 +1,90 @@
+// Conceptual Example.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+/**
+* The Target defines the domain specific interface used by the client code.
+*/
+class Target 
+{
+public:
+    virtual ~Target() = default;
+
+    virtual std::string Request() const
+    {
+        return "Target: The default target's behavior.";
+    }
+};
+
+/**
+ *The Adaptee contains some usefull behavior, but its interface is incompatible with the existing 
+ *client code. THe Adaptee needs some adaptation before the client code can use it.
+*/
+class Adaptee
+{
+public:
+    std::string SpecificRequest() const
+    {
+        return ".eetpadA eht fo roivaheb laicepS";
+    }
+};
+
+/**
+ *The Adapter makes the Adaptee's interface compatible with the Target's interface.
+*/
+class Adapter : public Target
+{
+private:
+    Adaptee *adaptee_;
+public:
+    Adapter (Adaptee* adaptee) : adaptee_(adaptee)
+    {}
+    std::string Request() const override
+    {
+        std::string to_reverse = adaptee_->SpecificRequest();
+        std::reverse(to_reverse.begin(), to_reverse.end());
+        return "Adapter: (TRANSLATED) " + to_reverse;
+    }
+};
+
+/**
+ *The client code supports all classes that follow the Target interface.
+*/
+void ClientCode(Target* target)
+{
+    std::cout << target->Request();
+}
+
+int main()
+{
+    std::cout << "Client: I can work just fine with the Target objects:\n";
+    Target* target = new Target;
+    ClientCode(target);
+    std::cout << "\n\n";
+    Adaptee* adaptee = new Adaptee;
+    std::cout << "Client: The Adaptee class has a weird interface. See, I don't understand it:\n";
+    std::cout << "\n\n";
+    std::cout << "Client: But I can wordk with it via the Adapter:\n";
+    Adapter* adapter = new Adapter(adaptee);
+    ClientCode(adapter);
+    std::cout << "\n";
+
+    delete target;
+    delete adaptee;
+    delete adapter;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
